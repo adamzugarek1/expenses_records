a8c5ef51433494c83a696e6a0b7fdaf89dc7547b
OlekZa <113592194+OlekZa@users.noreply.github.com>
2024-01-11 11:23:48 +0100

    Deleting old, adding new

--- vydaje/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-from . import views
-
-urlpatterns = [path('login/', LoginUser.as_view(), name='login'),
-               path('logout/', logout_user, name='logout'),
-               path('register/', RegisterUser.as_view(), name='register'),
-               path('rates/<slug:rates_slug>/', ShowRate.as_view(), name='post'),
-               ]
